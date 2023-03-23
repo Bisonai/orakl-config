ae328afb22f9ae76acf60ab5999918266671fbe4
bayram98 <bayramguwanjow@gmail.com>
2023-03-23 15:53:37 +0900

    feat: add to show Baobab Aggregator List

--- script/generateReadme.py
+++ script/generateReadme.py
@@ -1,14 +1,16 @@
 import os
 import json
 
-adapterPath = '/Users/bayram/Git/orakl-config/adapter'
-aggregatorPath = '/Users/bayram/Git/orakl-config/aggregator/default'
+adapterPath = 'adapter/'
+aggregatorDefaultPath = 'aggregator/default/'
+aggregatorBaobabPath = 'aggregator/baobab/'
 
 adapters = sorted(os.listdir(adapterPath))
-aggregators = sorted(os.listdir(aggregatorPath))
+aggregatorsDefault = sorted(os.listdir(aggregatorDefaultPath))
+aggregatorsBaobab = sorted(os.listdir(aggregatorBaobabPath))
 
-adapterUrl = "https://bisonai.github.io/orakl-config/adapter/"
-aggregatorUrl = "https://bisonai.github.io/orakl-config/aggregator/default/"
+# adapterUrl = "https://bisonai.github.io/orakl-config/adapter/"
+# aggregatorUrl = "https://bisonai.github.io/orakl-config/aggregator/default/"
 
 
 def makeLine(words):
@@ -46,25 +48,25 @@ def generateAdapterList():
             if key == 'feeds':
                 values.append(len(data[key]))
             elif key == 'name':
-                values.append({'url': adapterUrl + adapter, 'value': data[key]})
+                values.append({'url': filePath, 'value': data[key]})
             else:
                 values.append(data[key])
         makeLine(values)
 
 
-def generateAggregatorList():
+def generateAggregatorList(aggregatorBasePath, aggregators):
     keys = ['name', 'aggregatorHash', 'address', 'heartbeat', 'threshold', 'absoluteThreshold', 'adapterHash']
     makeLine(keys)
     makeEmptyLine(keys)
     for aggregator in aggregators:
-        filePath = os.path.join(aggregatorPath, aggregator)
+        filePath = os.path.join(aggregatorBasePath, aggregator)
         data = loadJsonFromPath(filePath)
         values = []
         for key in keys:
             if key == 'feeds':
                 values.append(len(data[key]))
             elif key == 'name':
-                values.append({'url': aggregatorUrl + aggregator, 'value': data[key]})
+                values.append({'url': filePath, 'value': data[key]})
             else:
                 values.append(data[key])
         makeLine(values)
@@ -76,8 +78,8 @@ def checkHashMatch():
         adapterFilePath = os.path.join(adapterPath, adapter)
         adapterData = loadJsonFromPath(adapterFilePath)
 
-        aggregator = aggregators[i]
-        aggregatorFilePath = os.path.join(aggregatorPath, aggregator)
+        aggregator = aggregatorsDefault[i]
+        aggregatorFilePath = os.path.join(aggregatorDefaultPath, aggregator)
         aggregatorData = loadJsonFromPath(aggregatorFilePath)
         if adapterData['name'] != aggregatorData['name']:
             msg = "Wrong file Name or Price feed name, Adapter:{} Aggregator: {}".format(adapter, aggregator)
@@ -98,5 +100,7 @@ checkHashMatch()
 print('# Orakl Config\n')
 print('## Adapter List\n')
 generateAdapterList()
-print('## Aggregator List\n')
-generateAggregatorList()
+print('## Aggregator Baobab\n')
+generateAggregatorList(aggregatorDefaultPath, aggregatorsBaobab)
+print('## Aggregator Default\n')
+generateAggregatorList(aggregatorDefaultPath, aggregatorsDefault)
