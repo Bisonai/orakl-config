8f908832ab6b60f9dfcdd2237d3614e284c39232
Martin Kersner <martin@bisonai.com>
2024-03-13 16:16:27 +0900

    fix: keep adapter files sorted

--- script/collect-adapter-files.py
+++ script/collect-adapter-files.py
@@ -8,18 +8,18 @@ def load_json_from_path(file_path: Path):
         return json.load(json_file)
 
 
-def collect_adapter_files(adapter_dir: str, output_file_path: str):
-    adapter_dir = Path(adapter_dir)
-    adapter_files = list(adapter_dir.glob("*.json"))
+def collect_adapter_files(adapter_dir: Path, output_file_path: str):
+    adapter_files = sorted(adapter_dir.glob("*.json"))
 
     result = []
     for file in adapter_files:
-        if file.name != "por.json":
+        if "por.json" not in file.name:
             result.append(load_json_from_path(file))
 
     with open(output_file_path, "w") as f:
         json.dump({"adapters": result}, f, indent=4)
 
 
-collect_adapter_files("adapter/baobab", "baobab_adapters.json")
-collect_adapter_files("adapter/cypress", "cypress_adapters.json")
+if __name__ == "__main__":
+    collect_adapter_files(Path("adapter/baobab"), "baobab_adapters.json")
+    collect_adapter_files(Path("adapter/cypress"), "cypress_adapters.json")
