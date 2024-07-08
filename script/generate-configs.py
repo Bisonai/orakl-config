c05057f77f26fa692664393e66c3a10ae4979518
Intizar Tashov <intizar.tashov22@gmail.com>
2024-07-08 15:00:51 +0900

    increase submission interval from 15seconds to 1 hour (#100)
    * increase submission interval from 15seconds to 1 hour
    
    * collect files

--- script/generate-configs.py
+++ script/generate-configs.py
@@ -9,7 +9,7 @@ SYMBOLS_PATH = Path("tmp/symbols.json")
 
 DEFAULT_FETCH_INTERVAL = 2000
 DEFAULT_AGGREGATE_INTERVAL = 400
-DEFAULT_SUBMIT_INTERVAL = 15000
+DEFAULT_SUBMIT_INTERVAL = 3_600_000 # 1 hour
 
 def load_json_from_url(url):
     response = requests.get(url)
